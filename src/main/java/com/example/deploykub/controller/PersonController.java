48816173095cf64d7333db0781817e6ab06307ee
Lahcen JOU <lahcen.jou@eviden.com>
2024-09-30 13:59:46 +0100

    post person

--- src/main/java/com/example/deploykub/controller/PersonController.java
+++ src/main/java/com/example/deploykub/controller/PersonController.java
@@ -5,6 +5,8 @@ import com.example.deploykub.entity.Person;
 import com.example.deploykub.repository.PersonRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,5 +20,11 @@ public class PersonController {
     @GetMapping("/getPersons")
     private List<Person> getPersons() {
         return personRepository.findAll();
+    }
+
+
+    @PostMapping("/createPerson")
+    private Person createPerson(@RequestBody Person person){
+        return personRepository.save(person);
     }
 }
